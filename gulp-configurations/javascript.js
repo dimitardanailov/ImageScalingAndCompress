c9b8869b92ccc6ed91693f0759b83d91b136900d
Dimitar Danailov <dimitar.danailov@mentormate.com>
2015-11-17 18:57:59 +0200

    We have implementation for javascript concat and watching of file changes.

--- gulp-configurations/javascript.js
+++ gulp-configurations/javascript.js
@@ -1,6 +1,4 @@
-import gulp from 'gulp';
-
-const jsConfigurations = {
+const configurations = {
 	'folderStructure' : {
 		'baseDevelopment': './js/development',
 		'baseProduction': './js/production'
@@ -8,9 +6,44 @@ const jsConfigurations = {
 };
 
 // Entties
-jsConfigurations.folderStructure['entities'] = {
-	'base': `${jsConfigurations.folderStructure.baseDevelopment}/entities`
+configurations.folderStructure['entities'] = {
+	'base': `${configurations.folderStructure.baseDevelopment}/entities`
 };
-jsConfigurations.folderStructure.entities['htmlContainers'] = `${jsConfigurations.folderStructure.entities.base}/htmlContainers`;
+configurations.folderStructure.entities['htmlContainers'] = `${configurations.folderStructure.entities.base}/htmlContainers`;
 
-exports.configuration = jsConfigurations;
+// Concatenation
+configurations.concatenationLocations = {
+	'mainfile' : 'application.min.js',
+	'base' : {
+		'libraries': 'libraries.min.js',
+		'projects': 'project.min.js'
+	}, 
+	'temp' : {
+		'entities': 'entities.min.js'
+	}
+};
+
+configurations.baseFiles = generateTempFiles('base');
+configurations.tempFiles = generateTempFiles('temp');
+
+configurations.minifyLocations = {
+	'libraries': configurations.folderStructure.baseProduction + '/' + configurations.concatenationLocations.base.libraries,
+	'projects': configurations.folderStructure.baseProduction + '/' + configurations.concatenationLocations.base.projects
+};
+
+function generateTempFiles(concatenationLocationsKey) {
+	const baseProductionFilePath = configurations.folderStructure.baseProduction.replace('./', '') + '/';
+	const tempFiles = [];
+
+	let tempLocation = null;
+	// http://stackoverflow.com/questions/921789/how-to-loop-through-javascript-object-literal-with-objects-as-members
+	Object.keys(configurations.concatenationLocations[concatenationLocationsKey]).forEach(function(key) {
+		tempLocation = baseProductionFilePath + configurations.concatenationLocations[concatenationLocationsKey][key];
+		
+		tempFiles.push(tempLocation);
+	});
+
+	return tempFiles;
+}
+
+exports.configuration = configurations;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,9 @@ import gutil from 'gulp-util';
 // Babel
 import babel from 'gulp-babel';
 
+// Tracking files changes
+import watch from 'gulp-watch';
+
 //Imagemin
 import gulpimagemin from 'gulp-imagemin';
 import imageminpngquant from 'imagemin-pngquant';
@@ -29,20 +32,106 @@ import minifyCss from 'gulp-minify-css';
 
 //Custom Modules
 import gulpErrorHandling from './gulp-error-handling'
-import jsConfigurations from './gulp-configurations/javascript.js'
+import js from './gulp-configurations/javascript.js'
 
 gulp.task('babel:entities', () => {
+	return streamqueue
+		(
+			{ objectMode: true },
+			gulp.src(js.configuration.folderStructure.entities.htmlContainers + '/BaseContainer.js'),
+			gulp.src(js.configuration.folderStructure.entities.htmlContainers + '/ImageCompressionContainer.js'),
+			gulp.src(js.configuration.folderStructure.entities.htmlContainers + '/ImageCompressionStatistics.js')
+		)
+		.pipe(babel())
+		.pipe(wrap('//<%= file.path %>\n<%= contents %>'))
+		// We will use .min prefix, because we want do not have problems with html include.
+		.pipe(concat(js.configuration.concatenationLocations.temp.entities))
+		.pipe(gulp.dest(js.configuration.folderStructure.baseProduction))
+		.on('error', gulpErrorHandling.onWarning);
+});
+
+/**
+ * Task will contact all javascript project files.
+ */
+gulp.task('concat:javascript-project-files', ['babel:entities'], () => {
+	return gulp.src(js.configuration.folderStructure.baseProduction + '/*.js')
+		.pipe(order(js.configuration.tempFiles, { base: './' }))
+		// We will use .min prefix, because we want do not have problems with html include.
+		.pipe(concat(js.configuration.concatenationLocations.base.projects))
+		.pipe(gulp.dest(js.configuration.folderStructure.baseProduction))
+		.on('error', gulpErrorHandling.onWarning);
+});
+
+/**
+ * Task will execute
+ * - first concatenation of javascript libraries
+ * - and after that we will append project styles
+ * - the last step: Merge libraries concatenation and project files.
+ */
+gulp.task('concat:javascript', ['concat:javascript-project-files'], () => {
 	return streamqueue({ objectMode: true },
-		gulp.src(jsConfigurations.configuration.folderStructure.entities.htmlContainers + '/BaseContainer.js'),
-		gulp.src(jsConfigurations.configuration.folderStructure.entities.htmlContainers + '/ImageCompressionContainer.js'),
-		gulp.src(jsConfigurations.configuration.folderStructure.entities.htmlContainers + '/ImageCompressionStatistics.js')
-	)
-	.pipe(babel())
-	.pipe(wrap('//<%= file.path %>\n<%= contents %>'))
-	// We will use .min prefix, because we want do not have problems with html include.
-	.pipe(concat('entities.min.js'))
-	.pipe(gulp.dest(jsConfigurations.configuration.folderStructure.baseProduction))
-	.on('error', gulpErrorHandling.onWarning);
+			gulp.src(js.configuration.minifyLocations.libraries),
+			gulp.src(js.configuration.minifyLocations.projects)
+		)
+		// We will use .min prefix, because we want do not have problems with html include.
+		.pipe(concat(js.configuration.concatenationLocations.mainfile))
+		.pipe(gulp.dest(js.configuration.folderStructure.baseProduction))
+		.on('error', gulpErrorHandling.onWarning);
+});
+
+/**
+ * Task will delete application production files.
+ */
+gulp.task('contact:clear-main-files', () => {
+	const javascriptMainFile = `${js.configuration.folderStructure.baseProduction}/${js.configuration.concatenationLocations.mainfile}`;
+
+	del([
+		javascriptMainFile
+	]);
+});
+
+/**
+  * Task will delete concatenation temp files.
+  */
+gulp.task('concat:clean-temp-javascript-files', () => {
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_operator
+	let javascriptFiles = js.configuration.baseFiles;
+	javascriptFiles.push(...js.configuration.tempFiles);
+
+	del(javascriptFiles, (error, paths) => {
+	});
+});
+
+gulp.task('concat:styling');
+
+/**
+  * This will run in this order:
+  * 1) contact:clear-main-files - Will prepare production directory
+  * 2) concat:styling and concat:javascript - in parallel
+  * 3) concat:clean-temp-javascript-files
+  * 4) Finally call the callback function
+  */
+gulp.task('concat', (callback) => {
+	runSequence(
+		'contact:clear-main-files',
+		['concat:styling', 'concat:javascript'],
+		'concat:clean-temp-javascript-files',
+		callback
+	);
+});
+
+/**
+  * Watch Files For Changes
+  */
+gulp.task('watch', function() {
+	// ** - If a "globstar" is alone in a path portion, then it matches zero or more directories and subdirectories searching for matches. 
+	// It does not crawl symlinked directories.
+	// Documentation: https://github.com/isaacs/node-glob
+	const trackingFiles = js.configuration.folderStructure.baseDevelopment + '/**';
+
+	// By default, errors during watch should not be fatal.
+	gulpErrorHandling.fatalLevel = gulpErrorHandling.fatalLevel || 'off';
+	gulp.watch(trackingFiles, ['concat']);
 });
 
 /**
@@ -59,13 +148,5 @@ gulp.task('imagemin:build', () => {
         .pipe(gulp.dest(''));
 });
 
-/**
- * Compile Our Sass
- */
-gulp.task('sass', () => {
-	return gulp.src('')
-		.pipe(sass({ indentedSyntax: true })
-			.on('error', gulperrorhandling.onError)
-		)
-		.pipe(gulp.dest('css'));
-});
+// Default Task
+gulp.task('default', ['concat']);
